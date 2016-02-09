cab50a224a5182531cba84b7f9beab2b2dbe406b
Chad Kruse <chadokruse@gmail.com>
2016-02-09 11:40:09 -0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,139 @@
+/*
+  Alpha by HTML5 UP
+  html5up.net | @n33co
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+  skel.breakpoints({
+    wide: '(max-width: 1680px)',
+    normal: '(max-width: 1280px)',
+    narrow: '(max-width: 980px)',
+    narrower: '(max-width: 840px)',
+    mobile: '(max-width: 736px)',
+    mobilep: '(max-width: 480px)'
+  });
+
+  $(function() {
+
+    var $window = $(window),
+      $body = $('body'),
+      $header = $('#header'),
+      $banner = $('#banner');
+
+    // Fix: Placeholder polyfill.
+      $('form').placeholder();
+
+    // Prioritize "important" elements on narrower.
+      skel.on('+narrower -narrower', function() {
+        $.prioritize(
+          '.important\\28 narrower\\29',
+          skel.breakpoint('narrower').active
+        );
+      });
+
+    // Dropdowns.
+      $('#nav > ul').dropotron({
+        alignment: 'right'
+      });
+
+    // Off-Canvas Navigation.
+
+      // Navigation Button.
+        $(
+          '<div id="navButton">' +
+            '<a href="#navPanel" class="toggle"></a>' +
+          '</div>'
+        )
+          .appendTo($body);
+
+      // Navigation Panel.
+        $(
+          '<div id="navPanel">' +
+            '<nav>' +
+              $('#nav').navList() +
+            '</nav>' +
+          '</div>'
+        )
+          .appendTo($body)
+          .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'left',
+            target: $body,
+            visibleClass: 'navPanel-visible'
+          });
+
+      // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+          $('#navButton, #navPanel, #page-wrapper')
+            .css('transition', 'none');
+
+    // Header.
+    // If the header is using "alt" styling and #banner is present, use scrollwatch
+    // to revert it back to normal styling once the user scrolls past the banner.
+    // Note: This is disabled on mobile devices.
+      if (!skel.vars.mobile
+      &&  $header.hasClass('alt')
+      &&  $banner.length > 0) {
+
+        $window.on('load', function() {
+
+          $banner.scrollwatch({
+            delay:    0,
+            range:    0.5,
+            anchor:   'top',
+            on:     function() { $header.addClass('alt reveal'); },
+            off:    function() { $header.removeClass('alt'); }
+          });
+
+        });
+
+      }
+
+  });
+
+  $(function() {
+    // Format numbers
+      // Docs: https://github.com/customd/jquery-number
+      $('[data-grant-amount]')
+        .number( true )
+        .prepend( '$' );
+
+    // Grant totals
+      var array = {};
+      var group;
+      var amount;
+      var total = 0;
+
+      $('td[data-grant-group-name]').each(function(i, el){
+
+          group = $(el).data('grant-group-name');
+          amount = $(el).data('grant-amount');
+
+          if (array.hasOwnProperty(group)) {
+              array[group] += amount;
+          } else {
+              array[group] = amount;
+          }
+          //console.log(array);
+      });
+
+      //Loop through array
+      $.each( array, function( key, value ) {
+        console.log('Key: ' + key);
+        console.log('Value: ' + value);
+
+        $('td[data-grant-group-total="'+ key +'"]')
+          .html(value.toFixed(0))
+          .number(true)
+          .prepend('$');
+
+      });
+  });
+
+})(jQuery);
